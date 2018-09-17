2155bead48c7ef158340d9cbafbe6222166ea274
PuniMeister <punimeister@gmail.com>
2018-09-17 17:01:48 +0900

    Update docker

---
+++ latest/webpack.config.js
@@ -0,0 +1,61 @@
+/*************************************************
+ * webpack.config.js
+ * @link https://webpack.js.org/configuration/
+ ************************************************/
+
+if (!process.env.ENTRY) {
+  console.log('Environment variable "ENTRY" is required.');
+  process.exit();
+}
+if (!process.env.OUT_DIR) {
+  console.log('Environment variable "OUT_DIR" is required.');
+  process.exit();
+}
+if (!process.env.OUT_FILE) {
+  console.log('Environment variable "OUT_FILE" is required.');
+  process.exit();
+}
+
+const path = require('path');
+const merge = require('webpack-merge');
+
+const commonConfig = {
+  entry: process.env.ENTRY,
+  output: {
+    path: path.resolve(__dirname, process.env.OUT_DIR),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/env'],
+        },
+      },
+    ],
+  },
+};
+
+const developmentConfig = {
+  mode: 'development',
+  output: {
+    filename: process.env.OUT_FILE,
+  },
+  devtool: 'source-map',
+  watch: true,
+  watchOptions: {
+    poll: 500,
+    ignored: /node_modules/,
+  },
+};
+
+const productionConfig = {
+  mode: 'production',
+  output: {
+    filename: process.env.OUT_FILE.replace(/(.+)\.js$/, '$1.min.js'),
+  },
+};
+
+module.exports = merge(commonConfig, process.env.MODE === 'production' ? productionConfig : developmentConfig);
--- latest/webpack.sh
+++ latest/webpack.sh
@@ -1,21 +1,10 @@
 #! /bin/sh
-if [ -z "${ENTRY}" ]; then
-  echo "Environment variable 'ENTRY' is required."
-  exit
+if [ ! -e /app/webpack/package.json ]; then
+  cp package.json webpack/
 fi
-if [ -z "${OUT_DIR}" ]; then
-  echo "Environment variable 'OUT_DIR' is required."
-  exit
+if [ ! -e /app/webpack/webpack.config.js ]; then
+  cp webpack.config.js webpack/
 fi
-if [ -z "${OUT_FILE}" ]; then
-  echo "Environment variable 'OUT_FILE' is required."
-  exit
-fi
-if [ "${MODE}" = "development" ]; then
-  npx webpack --config webpack.dev.js
-elif [ "${MODE}" = "production" ]; then
-  npx webpack --config webpack.prod.js
-else
-  echo "Environment variable 'MODE' is 'development' or 'production'."
-  exit
-fi
+cd webpack
+yarn install --no-progress
+npx webpack
